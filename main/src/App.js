6854d61b69804197c1e3f79a9a0b0a67f6bc89f8
Bhargavi <bhargavishetty113@gmail.com>
2021-08-28 19:54:26 +0530

    rightbar  added

--- main/src/App.js
+++ main/src/App.js
@@ -1,17 +1,34 @@
-import { makeStyles} from '@material-ui/core'
-import Navbar from './components/Navbar'
-const useStyles = makeStyles((theme)=>(
-  {
-
-  }
-))
+import {Grid, makeStyles} from '@material-ui/core'
+import Add from './componentss/Add'
+import Feed from './componentss/Feed'
+import Leftbar from './componentss/Leftbar'
+import Navbar from './componentss/Navbar'
+import Rightbar from './componentss/Rightbar'
+const useStyles = makeStyles((theme) => ({
+  right: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+}));
 
 function App() {
   const classes = useStyles()
   return (
     <div>
       <Navbar/>
-  
+      <Grid container>
+        <Grid item sm={2} xs={2}>
+          <Leftbar />
+        </Grid>
+        <Grid item sm={7} xs={10}>
+          <Feed />
+        </Grid>
+        <Grid item sm={3} className={classes.right}>
+          <Rightbar />
+        </Grid>
+        </Grid>
+        <Add/>
     </div>
   );
 }
